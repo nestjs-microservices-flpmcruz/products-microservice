c1803fa5ff51b4b63e390237614be7fed5779eab
Felipe <flpmireles@gmail.com>
2024-03-21 13:58:22 -0500

    first commit

---
+++ src/products/products.service.ts
@@ -0,0 +1,77 @@
+import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common';
+
+@Injectable()
+export class ProductsService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger(ProductsService.name);
+
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('Connected to the database');
+  }
+
+  create(createProductDto: CreateProductDto) {
+    return this.product.create({
+      data: createProductDto,
+    });
+  }
+
+  async findAll(paginationDto: PaginationDto) {
+    const { page, limit } = paginationDto;
+    const total = await this.product.count();
+    const lastPage = Math.ceil(total / limit);
+
+    return {
+      data: await this.product.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+        where: { available: true },
+      }),
+      meta: {
+        page,
+        total,
+        lastPage,
+      },
+    };
+  }
+
+  async findOne(id: number) {
+    const product = await this.product.findUnique({
+      where: { id, available: true},
+    });
+    
+    if (!product) {
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+    return product;
+  }
+
+  async update(updateProductDto: UpdateProductDto) {
+    const { id, ...data } = updateProductDto;
+    await this.findOne(id);
+    return this.product.update({
+      where: { id },
+      data: data,
+    });
+  }
+
+  async remove(id: number) {
+    await this.findOne(id);
+
+    const product = await this.product.update({
+      where: { id },
+      data: {
+        available: false,
+      },
+    });
+
+    return product;
+
+    return this.product.delete({
+      where: { id },
+    });
+  }
+}
